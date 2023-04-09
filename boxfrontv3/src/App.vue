9df5e845a7aecd45a2a64924178e72f8db916b84
zhoushoujian <1131769271@qq.com>
2023-04-09 09:18:31 +0800

    chore: add cicd

--- boxfrontv3/src/App.vue
+++ boxfrontv3/src/App.vue
@@ -1,67 +1,70 @@
 <template>
   <div>
-  <h1>Box Front</h1>
-  <!-- 保留2位小数 -->
-  <p>Roll: {{ roll }}</p>
-  <p>Pitch: {{ pitch }}</p>
-  <p>GPS Time: {{ gpsTime }}</p>
-  <p>GPS Status: {{ gpsStatus }}</p>
-  <p>Longitude: {{ longitude }}</p>
-  <p>Latitude: {{ latitude }}</p>
-  <p>Heading: {{ heading }}</p>
-  <p>Height: {{ height }} {{ heightUnit }}</p>
-  <p>Longitude Direction: {{ lonDirection }}</p>
-  <p>Latitude Direction: {{ latDirection }}</p>
-  <p>Speed: {{ speed }} m/s</p>
+    <h1>Box Front</h1>
+    <!-- 保留2位小数 -->
+    <p>Roll: {{ roll }}</p>
+    <p>Pitch: {{ pitch }}</p>
+    <p>GPS Time: {{ gpsTime }}</p>
+    <p>GPS Status: {{ gpsStatus }}</p>
+    <p>Longitude: {{ longitude }}</p>
+    <p>Latitude: {{ latitude }}</p>
+    <p>Heading: {{ heading }}</p>
+    <p>Height: {{ height }} {{ heightUnit }}</p>
+    <p>Longitude Direction: {{ lonDirection }}</p>
+    <p>Latitude Direction: {{ latDirection }}</p>
+    <p>Speed: {{ speed }} m/s</p>
   </div>
-  </template>
+</template>
   
-  <script>
-  export default {
+<script>
+export default {
   mounted() {
-  const socket = new WebSocket("ws://192.168.1.21:3000/ws/gyro");
-  const gpssocket = new WebSocket("ws://192.168.1.21:3000/ws/gps");
-  socket.onmessage = (event) => {
-  // turn string into object
-  const data = JSON.parse(event.data);
-  console.log(data);
-  // set data to template
-  this.roll = Math.round(data.roll * 100) / 100;
-  this.pitch = Math.round(data.pitch * 100) / 100;
-  };
-  gpssocket.onmessage = (event) => {
-  // turn string into object
-  const data = JSON.parse(event.data);
-  console.log(data);
-  // set data to template
-  this.gpsTime = data.GpsTime;
-  this.gpsStatus = data.Status;
-  this.longitude = data.Longitude;
-  this.latitude = data.Latitude;
-  this.heading = data.Heading;
-  this.height = data.Hight;
-  this.heightUnit = data.Hightude;
-  this.lonDirection = data.LonDirection;
-  this.latDirection = data.LatDirection;
-  this.speed = data.Speed;
-  };
+    // get server ip 
+    const ip = window.location.hostname;
+    // connect to websocket
+    const socket = new WebSocket("ws://"+ip+":3000/ws/gyro");
+    const gpssocket = new WebSocket("ws://"+ip+":3000/ws/gps");
+    socket.onmessage = (event) => {
+      // turn string into object
+      const data = JSON.parse(event.data);
+      console.log(data);
+      // set data to template
+      this.roll = Math.round(data.roll * 100) / 100;
+      this.pitch = Math.round(data.pitch * 100) / 100;
+    };
+    gpssocket.onmessage = (event) => {
+      // turn string into object
+      const data = JSON.parse(event.data);
+      console.log(data);
+      // set data to template
+      this.gpsTime = data.GpsTime;
+      this.gpsStatus = data.Status;
+      this.longitude = data.Longitude;
+      this.latitude = data.Latitude;
+      this.heading = data.Heading;
+      this.height = data.Hight;
+      this.heightUnit = data.Hightude;
+      this.lonDirection = data.LonDirection;
+      this.latDirection = data.LatDirection;
+      this.speed = data.Speed;
+    };
   },
   // show event data in template
   data() {
-  return {
-  roll: null,
-  pitch: null,
-  gpsTime: null,
-  gpsStatus: null,
-  longitude: null,
-  latitude: null,
-  heading: null,
-  height: null,
-  heightUnit: null,
-  lonDirection: null,
-  latDirection: null,
-  speed: null,
-  };
+    return {
+      roll: null,
+      pitch: null,
+      gpsTime: null,
+      gpsStatus: null,
+      longitude: null,
+      latitude: null,
+      heading: null,
+      height: null,
+      heightUnit: null,
+      lonDirection: null,
+      latDirection: null,
+      speed: null,
+    };
   },
-  };
-  </script>
+};
+</script>
